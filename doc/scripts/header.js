d186e375c8ad5d2e5456530db4764ac29a6352ed
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2021-07-26 23:39:19 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/587dd807680b9b47d58146ef8f24224d84b87d34/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,8 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href =
-		'https://coveralls.io/github/comparison-sorting/insertion-sort';
+	testlink.href = 'https://app.codecov.io/gh/comparison-sorting/insertion-sort';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
